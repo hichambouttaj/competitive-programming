5626de1cd804f36d0c463f5e423f5cb66e7e5b6c
HichamBouttaj <57833129+hichambouttaj@users.noreply.github.com>
2022-10-31 23:01:30 +0100

    Time: 24 ms (72.94%), Space: 17.4 MB (85.51%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
-    
-    bool isValid(int x, int y, int rows, int cols){
-        if(x >= rows || x < 0){
-            return false;
-        }
-        if(y >= cols || y < 0){
-            return false;
-        }
-        return true;
-    }
-    
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
         int rows = (int)matrix.size();
         int cols = (int)matrix[0].size();
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                if(isValid(i+1, j+1, rows, cols) && matrix[i][j] != matrix[i+1][j+1]){
+        for(int i = 0; i < rows - 1; i++){
+            for(int j = 0; j < cols - 1; j++){
+                if(matrix[i][j] != matrix[i+1][j+1]){
                     return false;
                 }
             }
