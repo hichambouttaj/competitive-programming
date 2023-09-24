d53bd938b06bf2122fff11d35e4f1fdd5dff5daa
HichamBouttaj <57833129+hichambouttaj@users.noreply.github.com>
2023-09-24 18:19:41 +0100

    Time: 5 ms (60.63%), Space: 8.5 MB (10.59%) - LeetHub

--- 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.cpp
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.cpp
@@ -2,31 +2,16 @@ class Solution {
 public:
     int maximumDifference(vector<int>& nums) {
         int sz = (int)nums.size();
-        vector<int> prefix_max(sz);
-
-        prefix_max[sz - 1] = 0;
-
-        for(int i = sz - 2; i >= 0; i--) {
-            if(prefix_max[i + 1] != 0) {
-                if(nums[i] < prefix_max[i + 1])
-                    prefix_max[i] = prefix_max[i + 1];
-                else
-                    prefix_max[i] = 0;
-            }else if(nums[i] < nums[i + 1]){
-                prefix_max[i] = nums[i + 1];
-            }else {
-                prefix_max[i] = 0;
-            }
-        }
-
         int rs = -1;
-
-        for(int i = 0; i < sz; i++) {
-            int sub = prefix_max[i] - nums[i];
-            if(sub > rs)
-                rs = sub;
-        }
-
+        int suffix_max = nums[sz - 1];
+        
+        for(int i = sz - 2; i >= 0; i--) {
+            if(nums[i] < suffix_max) {
+                rs = std::max(rs, suffix_max - nums[i]);
+            }
+            suffix_max = std::max(suffix_max, nums[i]);
+        } 
+        
         return rs;
     }
 };
