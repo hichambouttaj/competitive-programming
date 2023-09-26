d363b6e5a2d754d210d7f877c9b9fc64f0755148
HichamBouttaj <57833129+hichambouttaj@users.noreply.github.com>
2023-09-26 20:34:01 +0100

    Time: 21 ms (19.05%), Space: 18.7 MB (42.89%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
@@ -2,36 +2,20 @@ class Solution {
 public:
     int longestMountain(vector<int>& arr) {
         int sz = (int)arr.size();
+        int up_count = 0;
+        int down_count = 0;
         int max_length = 0;
-        int idx = 0;
-        int start, end;
         
-        while(idx < sz) {
-            start = idx;
-            
-            while(idx + 1 < sz && arr[idx] < arr[idx + 1]) {
-                idx++;
+        for(int i = 1; i < sz; i++) {
+            if(down_count && arr[i - 1] < arr[i] || arr[i - 1] == arr[i]) {
+                down_count = up_count = 0;
             }
+            up_count += arr[i - 1] < arr[i];
+            down_count += arr[i - 1] > arr[i];
             
-            if(start == idx) {
-                idx++;
-                continue;
+            if(down_count && up_count) {
+                max_length = std::max(max_length, up_count + down_count + 1);
             }
-            
-            end = idx;
-            
-            while(idx + 1 < sz && arr[idx] > arr[idx + 1]) {
-                idx++;
-            }
-            
-            if(idx == end) {
-                ++idx;
-                continue;
-            }
-            
-            end = idx;
-            
-            max_length = std::max(max_length, end - start + 1);
         }
         
         return max_length;
