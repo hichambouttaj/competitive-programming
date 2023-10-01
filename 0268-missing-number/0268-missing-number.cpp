a76721f0328a986c2ca49984dad9535e592d5121
HichamBouttaj <57833129+hichambouttaj@users.noreply.github.com>
2023-10-01 19:24:44 +0100

    Time: 12 ms (73.41%), Space: 18.2 MB (22.37%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,13 +3,12 @@ public:
     int missingNumber(vector<int>& nums) {
         int n = (int)nums.size();
         
-        int sum_nums = 0;
         int sum_range = (n * (n + 1)) / 2;
         
         for(int i = 0; i < n; i++) {
-            sum_nums += nums[i];
+            sum_range -= nums[i];
         }
         
-        return sum_range - sum_nums;        
+        return sum_range;        
     }
 };
