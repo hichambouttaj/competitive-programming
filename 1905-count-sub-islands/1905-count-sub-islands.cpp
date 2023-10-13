191aad96742313b3fc4b1a3f9c8b7050a06fc102
HichamBouttaj <57833129+hichambouttaj@users.noreply.github.com>
2023-10-13 20:29:48 +0100

    Time: 237 ms (69.89%), Space: 87.7 MB (96.93%) - LeetHub

---
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -0,0 +1,51 @@
+class Solution {
+private:
+    bool isSubIslands;
+    int dx[4] {0, 0, 1, -1};
+    int dy[4] {1, -1, 0, 0};
+public:
+    bool isValid(int r, int c, vector<vector<int>> &grid2){
+        if(r < 0 || r >= (int)grid2.size()){
+            return false;
+        }
+        if(c < 0 || c >= (int)grid2[0].size()){
+            return false;
+        }
+        return true;
+    }
+    
+    void dfs(int r, int c, vector<vector<int>> &grid1, vector<vector<int>> &grid2){
+        if(!isValid(r, c, grid2) || grid2[r][c] == 0){
+            return;
+        }
+        
+        if(grid2[r][c] && !grid1[r][c]){
+            isSubIslands = false;
+            return;
+        }
+        grid2[r][c] = 0;
+        
+
+        for(int d = 0; d < 4; d++){
+            dfs(r + dx[d], c + dy[d], grid1, grid2);
+        }
+        
+    }
+    
+    
+    int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
+        int sx = (int)grid2.size();
+        int sy = (int)grid2[0].size();
+        int cnt = 0;
+        for(int i = 0; i < sx; i++){
+            for(int j = 0; j < sy; j++){
+                if(grid2[i][j]){
+                    isSubIslands = true;
+                    dfs(i, j, grid1, grid2);
+                    cnt += isSubIslands;
+                }
+            }
+        }
+        return cnt;
+    }
+};
