6cb421113d4f343d7b9b5f17aa0322e6e1e61869
HichamBouttaj <57833129+hichambouttaj@users.noreply.github.com>
2023-11-10 17:46:30 +0100

    Time: 16 ms (73.64%), Space: 19.7 MB (18.30%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int sz = (int)nums.size();
+        int n = (int)nums.size();
+        int left = 0;
+        int right = 0;
         
-        int non_zeros_count = 0;
-        
-        for(int i = 0; i < sz; i++) {
-            if(nums[i] != 0) {
-                if(i != non_zeros_count) {
-                    nums[non_zeros_count] = nums[i];
-                    nums[i] = 0;
-                }
-                non_zeros_count++;
+        while(right < n) {
+            if(nums[right] != 0) {
+                nums[left++] = nums[right++];
+            }else {
+                right++;
             }
         }
+        
+        while(left < n)
+            nums[left++] = 0;
     }
 };
